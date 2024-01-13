3b88a9af066b29975fc05e362a0872af37fd6675
Leesubin <72747720+leesubin97@users.noreply.github.com>
2024-01-13 17:30:03 +0900

    Update 피로도.java
    [fix] 주석추가

--- 프로그래머스/Lv.2/87946. 피로도/피로도.java
+++ 프로그래머스/Lv.2/87946. 피로도/피로도.java
@@ -8,11 +8,14 @@ class Solution {
         boolean[] experDungeonsState = new boolean[dungeons.length];
         return experDangeons(k, dungeons, experDungeonsState);
     }
-    
+
     private int experDangeons(int k, int[][] dungeons, boolean[] visited){
+        // 탐험 가능 최대 던전 수
         int maxExperDangeons = 0;
+        
+        // 모든 던전 탐색
         for(int i = 0 ; i < dungeons.length; i++){
-            // 던전 경험을 안한 경우 탐색
+            // 현재 피로도 기준으로 던전 경험을 안한 경우 탐색
             if(!visited[i] && dungeons[i][0] <= k){
                 visited[i] = true;
                 maxExperDangeons = Math.max(maxExperDangeons, 1 + experDangeons(k-dungeons[i][1], dungeons, visited));
